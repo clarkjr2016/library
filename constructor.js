c1cbab593608adc7cebd7aa618cff431742e08e6
Eugene Clark <clarkjr2016@gmail.com>
2023-04-22 07:51:57 -0400

    Added wrap capabilties for books and added hide form button

--- constructor.js
+++ constructor.js
@@ -15,15 +15,14 @@ const thinkAndGrowRich = new Book(
   "Think and Grow Rich",
   "Napolean Hill",
   394,
-  "Did read"
+  true
 );
 
-const meditations = new Book(
-  "Meditations",
-  "Marcus Arelius",
-  256,
-  "Have not read"
-);
+const meditations = new Book("Meditations", "Marcus Arelius", 256, false);
+
+const ego = new Book("Ego is the Enemy", "Ryan Holiday", 264, true);
+
+const nineteenEightyFour = new Book("1984", "George Orwell", 376, true);
 
 function addBookToLibrary(book) {
   library.push(book);
@@ -31,6 +30,8 @@ function addBookToLibrary(book) {
 
 addBookToLibrary(thinkAndGrowRich);
 addBookToLibrary(meditations);
+addBookToLibrary(ego);
+addBookToLibrary(nineteenEightyFour);
 
 const cardContainer = document.querySelector(".card-container"); // creating variable for card container to append child elements to
 
@@ -39,8 +40,18 @@ function displayBooks() {
     const card = document.createElement("div"); // creates a div element that will represent the cards that book information will be displayed on
     cardContainer.appendChild(card); // this appends the card to the card-container div so that the cards will be displayed
     const title = document.createElement("p"); // this creates a p element for which the title will be displayed
-    title.innerHTML = book.title; // this sets the paragraph content to the current object's title property value
-    card.appendChild(title); // this appends that value to the caed
+    title.innerHTML = `Book Title: ${book.title}`; // this sets the paragraph content to the current object's title property value
+    card.appendChild(title); // this appends that value to the card
+    const author = document.createElement("p");
+    author.innerHTML = `Author: ${book.author}`;
+    card.appendChild(author);
+    const pages = document.createElement("p");
+    pages.innerHTML = `Number of pages: ${book.pages}`;
+    card.appendChild(pages);
+    const read = document.createElement("p");
+    read.innerHTML = book.read;
+    card.appendChild(read);
+    card.classList.add("card"); // adding the card class to the card elements
   });
   // utilize this function to loop through library array and display the values as cards in html elements
 }
